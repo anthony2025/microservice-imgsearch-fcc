e758d57a21f0cfc603f1bd1c4612ae1188fd36e4
Anthony Ascencio <anthony2025@gmail.com>
2017-01-09 01:45:54 -0500

    service working

--- src/searchRouter.js
+++ src/searchRouter.js
@@ -1,16 +1,42 @@
-const searchController = require('./searchController')
+const request = require('request')
 const RecentTerm = require('./recentModel')
 const SearchRouter = require('express').Router()
 
 SearchRouter.get('/:query', (req, res) => {
   const query = req.params.query
-  const offset = req.params.offset
-
-  searchController.save(query, RecentTerm)
-  const response = searchController.get(query, offset)
-  if (!response) res.status(500).send('api didnt return a valid response')
-  res.status(200).json(response)
+  const offset = req.query.offset || 0
+  console.log(offset)
+  const options = {
+    url: `https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${query}&count=10&offset=${offset}`,
+    headers: {'Ocp-Apim-Subscription-Key': process.env.APIKEY}
+  }
+  request.get(options, (err, response, body) => {
+    if (err) return err
+    if (!response) res.status(500).end()
+    res.json(filterJSON(body))
+  })
+  saveRecentTerm(query)
 })
+
+function saveRecentTerm (term) {
+  const recent = new RecentTerm()
+  recent.term = term
+  recent.time = new Date(Date.now()).toUTCString()
+  recent.save()
+}
+
+function filterJSON (data) {
+  let listOfImages = []
+  JSON.parse(data).value.forEach((img) => {
+    let image = {}
+    image.url = img.contentUrl
+    image.snippet = img.name
+    image.thumbnail = img.thumbnailUrl
+    image.context = img.hostPageUrl
+    listOfImages.push(image)
+  })
+  return listOfImages
+}
 
 module.exports = SearchRouter
 
